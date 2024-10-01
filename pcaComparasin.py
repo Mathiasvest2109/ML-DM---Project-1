9a15e8b22e988a25a35fd5c041e942699b1c5773
jemborg123 <s235251@dtu.dk>
2024-10-01 23:28:28 +0200

    modified the histograms to look nicer, changed the PCA analysis in analysis file to only show the graphs with unit variance, made pcaComparasin which compares all the PC's to each other

--- pcaComparasin.py
+++ pcaComparasin.py
@@ -65,63 +65,38 @@ j = 1
 plt.figure(figsize=(6, 10))
 plt.subplots_adjust(hspace=0.6)
 plt.title("Heart disease with regard to "+chosenfeature+": Effect of standardization")
-nrows = 3
-ncols = 1
-for k in range(1):
-    k=1
-    # Obtain the PCA solution by calculate the SVD of either Y1 or Y2
-    U, S, Vh = svd(Ys[k], full_matrices=False)
-    V = Vh.T  # For the direction of V to fit the convention in the course we transpose
-    # For visualization purposes, we flip the directionality of the
-    # principal directions such that the directions match for Y1 and Y2.
-    if k == 1:
-        V = -V
-        U = -U
+nrows = 13
+ncols = 13
 
-    # Compute variance explained
-    rho = (S * S) / (S * S).sum()
+k=1
+# Obtain the PCA solution by calculate the SVD of either Y1 or Y2
+U, S, Vh = svd(Ys[k], full_matrices=False)
+V = Vh.T  # For the direction of V to fit the convention in the course we transpose
+# For visualization purposes, we flip the directionality of the
+# principal directions such that the directions match for Y1 and Y2.
 
-    # Compute the projection onto the principal components
-    Z = U * S
+V = -V
+U = -U
 
-    # Plot projection
-    plt.subplot(nrows, ncols, 0 + k)
-    C = len(classnames)
-    for c in range(C):
-        plt.plot(Z[y == c, i], Z[y == c, j], ".", alpha=0.5)
-    plt.xlabel("PC" + str(i + 1))
-    plt.xlabel("PC" + str(j + 1))
-    plt.title(titles[k] + "\n" + "Projection")
-    plt.legend(classnames)
-    plt.axis("equal")
+# Compute variance explained
+rho = (S * S) / (S * S).sum()
 
-    # Plot attribute coefficients in principal component space
-    plt.subplot(nrows, ncols, 1 + k)
-    for att in range(V.shape[1]):
-        plt.arrow(0, 0, V[att, i], V[att, j])
-        plt.text(V[att, i], V[att, j], attributeNames[att])
-    plt.xlim([-1, 1])
-    plt.ylim([-1, 1])
-    plt.xlabel("PC" + str(i + 1))
-    plt.ylabel("PC" + str(j + 1))
-    plt.grid()
-    # Add a unit circle
-    plt.plot(
-        np.cos(np.arange(0, 2 * np.pi, 0.01)), np.sin(np.arange(0, 2 * np.pi, 0.01))
-    )
-    plt.title(titles[k] + "\n" + "Attribute coefficients")
-    plt.axis("equal")
+# Compute the projection onto the principal components
+Z = U * S
 
-    # Plot cumulative variance explained
-    plt.subplot(nrows, ncols, 2 + k)
-    plt.plot(range(1, len(rho) + 1), rho, "x-")
-    plt.plot(range(1, len(rho) + 1), np.cumsum(rho), "o-")
-    plt.plot([1, len(rho)], [threshold, threshold], "k--")
-    plt.title("Variance explained by principal components")
-    plt.xlabel("Principal component")
-    plt.ylabel("Variance explained")
-    plt.legend(["Individual", "Cumulative", "Threshold"])
-    plt.grid()
-    plt.title(titles[k] + "\n" + "Variance explained")
-
+# Plot projection
+for yaxis in range(13):
+    for xaxis in range (13):
+        plt.subplot(nrows, ncols, xaxis*13+yaxis+1)
+        C = len(classnames)
+        for c in range(C):
+            plt.plot(Z[y == c, xaxis], Z[y == c, yaxis], ".", alpha=0.5)
+        if (yaxis == 0): plt.ylabel("PC" + str(xaxis + 1))
+        if (xaxis == 12): plt.xlabel("PC" + str(yaxis + 1))
+        plt.xticks([])  
+        plt.yticks([])
+        
+        #plt.legend(classnames)
+        plt.axis("equal")
+plt.suptitle(titles[k] + "\n" + "Projection")
 plt.show()
